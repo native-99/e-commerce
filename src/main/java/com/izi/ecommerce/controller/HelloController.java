e418cc6838d8f1de24227f47c8b9212ffc973e64
native-99 <native-99@users.noreply.github.com>
2026-06-18 06:18:44 +0700

    Initial Commit

---
+++ src/main/java/com/izi/ecommerce/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.izi.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class HelloController {
+
+    public String Hello(){
+        return
+    }
+}
---
+++ src/test/java/com/izi/ecommerce/WebEcommerceAppTests.java
@@ -0,0 +1,13 @@
+package com.izi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEcommerceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
